490f844b9dae89d7e79c585e4a6e402ef7e21b2b
Varasina Farmadani <sina@sinanonym.my.id>
2026-03-13 21:40:40 +0700

    feat(Profile): add ProfileCard components for profile information
    Signed-off-by: Varasina Farmadani <sina@sinanonym.my.id>

---
+++ composeApp/src/commonMain/kotlin/com/example/myprofileapp/ui/components/profile/ProfileCard.kt
@@ -0,0 +1,28 @@
+package com.example.myprofileapp.ui.components.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileCard(content: @Composable () -> Unit){
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            content()
+        }
+    }
+}
